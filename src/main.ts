3cf108a393da17dee0b50fd7b1e95017da877631
Hibikine Kage <goodsmile314159@gmail.com>
2019-07-19 01:56:25 +0900

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { wait } from '@hibikine/utils';
+const elements = document.getElementsByTagName('section');
+console.log(elements);
+for (let i = 0; i < elements.length; i += 1) {
+  console.log(elements[i]);
+}
